786ce562b3023bfbb6a93067c62940d13866de9a
Sindre Sorhus <sindresorhus@gmail.com>
2014-01-04 00:46:09 +0100

    update deps

--- test.js
+++ test.js
@@ -8,9 +8,11 @@ var reporter = require('./stylish.js').reporter;
 describe('jshint-stylish', function () {
 	it('should be used by JSHint', function () {
 		var ret = false;
-		var _log = process.stdout.write;
+		var _log = process.stdout.write.bind(process.stdout);
 
 		process.stdout.write = function (str) {
+			_log(str);
+
 			if (/line 8   col 1   'describe' is not defined/ig.test(chalk.stripColor(str || ''))) {
 				ret = true;
 			}
