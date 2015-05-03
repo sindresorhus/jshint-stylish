33502b836f74c986e7f280e8676f7741e8f1bae4
Sindre Sorhus <sindresorhus@gmail.com>
2015-05-03 18:04:37 +0200

    remove blur color windows hack now that chalk fixes it for us

--- stylish.js
+++ stylish.js
@@ -28,7 +28,7 @@ module.exports = {
 				'',
 				chalk.gray('line ' + err.line),
 				chalk.gray('col ' + err.character),
-				isError ? chalk.red(err.reason) : (process.platform !== 'win32' ? chalk.blue(err.reason) : chalk.cyan(err.reason))
+				isError ? chalk.red(err.reason) : chalk.blue(err.reason)
 			];
 
 			if (el.file !== prevfile) {
