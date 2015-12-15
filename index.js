f0f276785e68cd16f501f57b9ab000152978cbbb
Kiril Vatev <vatev.1@gmail.com>
2015-12-15 07:51:07 -0500

    using cyan instead of blue
    on Windows, even the lighter blue is too difficult to read

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ module.exports = {
 				'',
 				chalk.gray('line ' + err.line),
 				chalk.gray('col ' + err.character),
-				isError ? chalk.red(err.reason) : chalk.blue(err.reason)
+				isError ? chalk.red(err.reason) : chalk.cyan(err.reason)
 			];
 
 			if (el.file !== prevfile) {
